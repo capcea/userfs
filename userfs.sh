9224eb1e93016c148f683f3b087fc19c994c6be3
capcea <roman.aandreii@gmail.com>
2026-01-09 15:49:11 +0200

    logica pentru useri inactivi

--- userfs.sh
+++ userfs.sh
@@ -9,5 +9,17 @@ active=$(who | awk '{print $1}' | sort -u)  #stocare useri activi
 
 for u in $active; do
   mkdir -p "$ROOT/$u"  
-  ps -u "$u" > "$ROOT/$u/proc" 
+  ps -u "$u" > "$ROOT/$u/proc"
+  rm -f "$ROOT/$u/lastlogin" #sterge last login daca inca e activ
+done
+
+for d in "$ROOT"/*; do
+  [ -d "$d" ] || continue
+  user=$(basename "$d")
+
+  echo "$active" | grep -qx "$user"
+  if [ $? -ne 0 ]; then
+    : > "$d/proc"
+    lastlog -u "$user" > "$d/lastlogin" 2>/dev/null
+  fi
 done
