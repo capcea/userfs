db3b48bb08740afc9e16c6f3c99c7a4dd42efb7d
capcea <roman.aandreii@gmail.com>
2026-01-09 13:17:07 +0200

    creare fisier proc + comentarii

--- userfs.sh
+++ userfs.sh
@@ -3,8 +3,11 @@
 ROOT="UserFS"
 mkdir -p "$ROOT"
 
-active=$(who | awk '{print $1}' | sort -u) 
+active=$(who | awk '{print $1}' | sort -u)  #stocare useri activi
+
+#crearea folderului fiecarui user si fisierul proc din el
 
 for u in $active; do
-  mkdir -p "$ROOT/$u"
+  mkdir -p "$ROOT/$u"  
+  ps -u "$u" > "$ROOT/$u/proc" 
 done
